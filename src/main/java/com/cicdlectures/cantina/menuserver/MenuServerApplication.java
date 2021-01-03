5892e8bec68c243cfd37a60bfc77c245f568a87f
dduportal <damien.duportal@gmail.com>
2021-01-03 15:48:40 +0100

    Initialize repository with a Spring Initializer-generated empty application
    Signed-off-by: dduportal <damien.duportal@gmail.com>

---
+++ src/main/java/com/cicdlectures/cantina/menuserver/MenuServerApplication.java
@@ -0,0 +1,13 @@
+package com.cicdlectures.cantina.menuserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenuServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenuServerApplication.class, args);
+	}
+
+}
