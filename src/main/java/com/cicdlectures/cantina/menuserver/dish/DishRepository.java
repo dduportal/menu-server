81862e5b1df511971e5f7b77efe622cb35bdfb6e
dduportal <damien.duportal@gmail.com>
2021-01-03 17:04:38 +0100

    Add repositories for Dish and Menu
    Signed-off-by: dduportal <damien.duportal@gmail.com>

---
+++ src/main/java/com/cicdlectures/cantina/menuserver/dish/DishRepository.java
@@ -0,0 +1,6 @@
+package com.cicdlectures.cantina.menuserver.dish;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface DishRepository extends CrudRepository<Dish, Long> {
+}
---
+++ src/main/java/com/cicdlectures/cantina/menuserver/menu/MenuRepository.java
@@ -0,0 +1,6 @@
+package com.cicdlectures.cantina.menuserver.menu;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface MenuRepository extends CrudRepository<Menu, Long> {
+}
