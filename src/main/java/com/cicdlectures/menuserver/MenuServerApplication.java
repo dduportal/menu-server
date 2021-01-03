4b4b58b95946db7c73098f382d480591f6b30f1d
dduportal <damien.duportal@gmail.com>
2021-01-03 15:56:24 +0100

    init from tar.gz of slides/
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- src/main/java/com/cicdlectures/menuserver/MenuServerApplication.java
+++ src/main/java/com/cicdlectures/menuserver/MenuServerApplication.java
@@ -1,4 +1,4 @@
-package com.cicdlectures.cantina.menuserver;
+package com.cicdlectures.menuserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MenuServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MenuServerApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(MenuServerApplication.class, args);
+  }
 
 }
