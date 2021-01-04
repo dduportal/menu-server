1001a07d257d5f0bd969b0b590882d399494a0cb
Julien Levesy <jlevesy@gmail.com>
2021-01-04 20:42:58 +0100

    Add a MenuService that deals with creating the dishes and the menu

---
+++ src/main/java/com/cicdlectures/cantina/menuserver/menu/MenuService.java
@@ -0,0 +1,40 @@
+package com.cicdlectures.cantina.menuserver.menu;
+
+import com.cicdlectures.cantina.menuserver.dish.DishRepository;
+import com.cicdlectures.cantina.menuserver.dish.Dish;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MenuService {
+
+  @Autowired
+  private MenuRepository menuRepository;
+
+  @Autowired
+  private DishRepository dishRepository;
+
+  public Menu createMenu(Menu menu) {
+    Set<Dish> dishes = new HashSet<Dish>();
+
+    for(Dish dish: menu.getDishes()) {
+      Dish got = dishRepository.findByName(dish.getName());
+
+      if (got == null) {
+        got = dishRepository.save(dish);
+      }
+
+      dishes.add(got);
+    }
+
+    menu.setDishes(dishes);
+
+    menuRepository.save(menu);
+
+    return menuRepository.refresh(menu);
+  }
+}
